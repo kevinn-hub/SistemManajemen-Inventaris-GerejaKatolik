70264e4c1629a6f3865f18bd9b8ee995a2b8fa7f
kevinn-hub <2511500012@mahasiswa.atmaluhur.ac.id>
2026-06-11 09:12:30 +0700

    first commit

---
+++ gereja_katolik.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 11, 2026 at 02:11 AM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gereja_katolik`
+--
+CREATE DATABASE IF NOT EXISTS `gereja_katolik` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `gereja_katolik`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_divisi`
+--
+
+CREATE TABLE `tbl_divisi` (
+  `id_divisi` int(11) NOT NULL,
+  `nama_divisi` varchar(100) NOT NULL,
+  `penanggung_jawab` varchar(100) NOT NULL,
+  `status` enum('aktif','tidak aktif') DEFAULT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_divisi`
+--
+
+INSERT INTO `tbl_divisi` (`id_divisi`, `nama_divisi`, `penanggung_jawab`, `status`, `keterangan`) VALUES
+(1, 'Liturgi', 'Yohanes', 'aktif', 'ketua Hub. Masyarakat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `Id_user` int(5) NOT NULL,
+  `Username` varchar(50) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Role` enum('admin','pengurus','anggota') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`Id_user`, `Username`, `Password`, `Role`) VALUES
+(1, 'admin', 'admin', 'admin'),
+(2, 'pengurus', '1234', 'pengurus'),
+(3, 'anggota', 'anggota', 'anggota'),
+(4, '', '1234', 'pengurus');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_divisi`
+--
+ALTER TABLE `tbl_divisi`
+  ADD PRIMARY KEY (`id_divisi`);
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`Id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_divisi`
+--
+ALTER TABLE `tbl_divisi`
+  MODIFY `id_divisi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `Id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
